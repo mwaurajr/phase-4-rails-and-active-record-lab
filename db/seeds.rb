2907e404a1e8dbe8f32e7355a17d8c094a04f502
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-11-23 11:18:32 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: 'Cheddar', last_name: 'msee', grade: 9)
+Student.create!(first_name: 'Pepper Jack', last_name: 'msee wa', grade: 7)
+Student.create!(first_name: 'Limburger', last_name: 'msee wa matumbe', grade: 6)
